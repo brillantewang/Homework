370ac9afe022b2bf08755e473e5958a5a541366f
Brillante Wang <brillante.wang@gmail.com>
2017-11-15 06:53:49 -0800

    giphy homework

---
+++ W7D3/giphy/components/giphys_search.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import GiphysIndex from './giphys_index';
+
+class GiphysSearch extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { searchTerm: '' };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.fetchSearchGiphys(this.state.searchTerm);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" onChange={this.handleChange}/>
+        <input type="submit"/>
+      </form>
+    )
+  }
+}
+
+export default GiphysSearch;
